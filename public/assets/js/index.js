f0f1907dcb374eb4808954cb024218c664488250
lalitakapadia <lalita.kapadia@gmail.com>
2023-12-12 16:14:04 +0000

    working om saved notes and delete notes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,7 +5,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
   noteForm = document.querySelector('.note-form');
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -129,9 +129,8 @@ const handleRenderBtns = () => {
 
 // Render the list of note titles
 const renderNoteList = async (notes) => {
-  console.log(notes);
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes') {
+  if (window.location.pathname === '/notes.html') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -172,12 +171,14 @@ const renderNoteList = async (notes) => {
 
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
+    console.log(note.title);
     li.dataset.note = JSON.stringify(note);
 
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes') {
+
+  if (window.location.pathname === '/notes.html') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -185,7 +186,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === '/notes.html') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   clearBtn.addEventListener('click', renderActiveNote);
--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 //const notes = require('express').Router();
 
-const {v4:uuid} = require('uuid');
+const {v4:uuidv4} = require('uuid');
 
 const notes = express();
 
@@ -28,7 +28,7 @@ notes.post('/', (req, res) => {
     const newNotes = {
       title,
       text,
-      id: uuid.v4(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNotes, "./db/db.json");
@@ -40,5 +40,12 @@ notes.post('/', (req, res) => {
     res.json(response);
   }
 });
+
+// create a new rout for delete notes
+// it should receive a parameter, note id
+// read the db.json file
+// parse the db.json into notes json
+// remove the given note id from the notes json
+// write the file with remainig notes json
 
 module.exports = notes;
